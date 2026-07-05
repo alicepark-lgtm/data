562ce0b62935db120f24c1ffdbab648e9abee944
alicepark-lgtm <alicepark@goedu.kr>
2026-07-05 21:47:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
-st.title('성포고등학교')
-st.write('## 30101 홍길동')
+
+st.set_page_config(
+    page_title="성포고등학교",
+    page_icon="🏫",
+    layout="wide"
+)
+
+st.title("🏫 성포고등학교")
+st.write("## 30101 홍길동")
+
+st.success("왼쪽 사이드바에서 원하는 페이지를 선택하세요.")
