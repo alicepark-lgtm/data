95cb86dc97b950dc7a4e3ff85ced9c8138831b80
alicepark-lgtm <alicepark@goedu.kr>
2026-07-05 22:19:35 +0900

    Update 5_체크박스.py

--- pages/5_체크박스.py
+++ pages/5_체크박스.py
@@ -16,3 +16,14 @@ if coffee:
 
 if cola:
      st.write("여기 있어요 🥤")
+
+
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     '가장 좋아하는 색상은 무엇인가요',
+     ['초록', '노랑', '빨강', '파랑'],
+     ['노랑', '빨강'])
+
+st.write('당신이 선택한 색상:', options)
